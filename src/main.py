679aec83d9d756cb41eea098a8c4708a506d4430
Antonio <antonz@merlinx.com>
2022-05-23 03:11:05 +0300

    added color

--- src/main.py
+++ src/main.py
@@ -3,10 +3,15 @@ from pathlib import Path
 from DataNormalizer.CsvNormalizer import CsvNormalizer
 from Insights.BiggestDrop import BiggestIntradayDrop, BiggestDropFromOpenToClose
 
+
+def get_blue_string(string):
+    return "\033[94m" + string + "\033[0m"
+
+
 if __name__ == '__main__':
     this_file_path = Path(__file__).resolve()
-    csv_path = this_file_path.parent / Path('./StocksData/NasdaqComposite/IXIC.csv')
-    # csv_path = this_file_path.parent / Path('./StocksData/Nasdaq100/NDX.csv')
+    # csv_path = this_file_path.parent / Path('./StocksData/NasdaqComposite/IXIC.csv')
+    csv_path = this_file_path.parent / Path('./StocksData/Nasdaq100/NDX.csv')
     # csv_path = this_file_path.parent / Path('./StocksData/SP500/GSPC.csv')
 
     normalizer = CsvNormalizer(csv_path)
@@ -16,5 +21,5 @@ if __name__ == '__main__':
     for insight_obj in insights:
         insight = insight_obj.get_insight()
 
-        print(f"{insight_obj.__class__.__name__} {str(insight[0])} {str(insight[1])}")
+        print(f"{insight_obj.__class__.__name__} {str(insight[0])} {get_blue_string(str(insight[1]))}")
 
